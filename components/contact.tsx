f1282fe42deecc612feacd5a5e2fb9a79b0ab503
Faysal Abdiwahab <faysalrulz123@gmail.com>
2025-10-30 16:00:20 +1100

    Remove  airraid gender options from ContactSection form

--- components/contact.tsx
+++ components/contact.tsx
@@ -135,8 +135,6 @@ export default function ContactSection() {
                                     <SelectContent>
                                         <SelectItem value="male">Male</SelectItem>
                                         <SelectItem value="female">Female</SelectItem>
-                                        <SelectItem value="other">Other</SelectItem>
-                                        <SelectItem value="prefer-not-to-say">Prefer not to say</SelectItem>
                                     </SelectContent>
                                 </Select>
                             </div>
